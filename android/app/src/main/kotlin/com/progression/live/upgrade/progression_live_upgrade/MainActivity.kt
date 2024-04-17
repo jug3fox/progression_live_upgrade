bd6956a93bdd02df316693cbb66bb5e8e75cce63
jug3f <jug3fox@gmail.com>
2024-04-17 06:16:58 -0400

    First commit

---
+++ android/app/src/main/kotlin/com/progression/live/upgrade/progression_live_upgrade/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.progression.live.upgrade.progression_live_upgrade
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,216 @@
+import 'package:file_picker/file_picker.dart';
+import 'package:flutter/cupertino.dart';
+
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:google_ml_kit/google_ml_kit.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:pdf_text/pdf_text.dart';
+import 'package:google_mlkit_text_recognition/google_mlkit_text_recognition.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // TRY THIS: Try running your application with "flutter run". You'll see
+        // the application has a blue toolbar. Then, without quitting the app,
+        // try changing the seedColor in the colorScheme below to Colors.green
+        // and then invoke "hot reload" (save your changes or press the "hot
+        // reload" button in a Flutter-supported IDE, or press "r" if you used
+        // the command line to start the app).
+        //
+        // Notice that the counter didn't reset back to zero; the application
+        // state is not lost during the reload. To reset the state, use hot
+        // restart instead.
+        //
+        // This works for code too, not just values: Most code changes can be
+        // tested with just a hot reload.
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      // This call to setState tells the Flutter framework that something has
+      // changed in this State, which causes it to rerun the build method below
+      // so that the display can reflect the updated values. If we changed
+      // _counter without calling setState(), then the build method would not be
+      // called again, and so nothing would appear to happen.
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      appBar: AppBar(
+        // TRY THIS: Try changing the color here to a specific color (to
+        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
+        // change color while the other colors stay the same.
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
+      ),
+      body: true ? PDFScreen() : Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: Column(
+          // Column is also a layout widget. It takes a list of children and
+          // arranges them vertically. By default, it sizes itself to fit its
+          // children horizontally, and tries to be as tall as its parent.
+          //
+          // Column has various properties to control how it sizes itself and
+          // how it positions its children. Here we use mainAxisAlignment to
+          // center the children vertically; the main axis here is the vertical
+          // axis because Columns are vertical (the cross axis would be
+          // horizontal).
+          //
+          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
+          // action in the IDE, or press "p" in the console), to see the
+          // wireframe for each widget.
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class PDFScreen extends StatefulWidget {
+  const PDFScreen({super.key});
+
+  @override
+  PDFScreenState createState() => PDFScreenState();
+}
+
+class PDFScreenState extends State<PDFScreen> {
+  String _extractedText = '';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('PDF Text Recognition'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            ElevatedButton(
+              onPressed: _extractTextFromPDF,
+              child: Text('Extract Text from PDF'),
+            ),
+            SizedBox(height: 20),
+            Text(_extractedText),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Future _getFile() async {
+    print("get file");
+    FilePickerResult? result = await FilePicker.platform.pickFiles(allowMultiple: false);
+    if(result?.files.isNotEmpty == true) {
+      print("get file : ${result!.paths}");
+      //_extractTextFromPDF("/storage/emulated/0/Download/${result!.files[0].name}");
+    }
+  }
+
+  Future<void> _extractTextFromPDF() async {
+    try {
+      FilePickerResult? result = await FilePicker.platform.pickFiles(
+        type: FileType.custom,
+        allowedExtensions: ['pdf', 'jpg'],
+      );
+
+      if (result != null) {
+        PlatformFile file = result.files.first;
+        File pdfFile = File(file.path!);
+
+        PDFDoc pdfDoc = await PDFDoc.fromFile(pdfFile);
+
+        String text = '';
+        List<TextBlock> textBlocks = [];
+
+        for (int i = 0; i < pdfDoc.length; i++) {
+          PDFPage page = pdfDoc.pageAt(i+1);
+          text += await page.text;
+        }
+
+        // Use Google ML Kit Text Recognition to process extracted text
+        final inputImage = InputImage.fromFilePath(pdfFile.path);
+        final textDetector = TextRecognizer();
+        final RecognizedText recognizedText = await textDetector.processImage(
+            inputImage);
+
+        for (TextBlock block in recognizedText.blocks) {
+          textBlocks.add(block);
+        }
+
+        setState(() {
+          _extractedText = recognizedText.text;
+        });
+      }
+    } catch (e) {
+      print('Error: $e');
+    }
+  }
+}
+
